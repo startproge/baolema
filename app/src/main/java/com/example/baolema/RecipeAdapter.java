5a959096dd74752be3ba432c60fcc47682acbb1d
startproge <2388998657@qq.com>
2019-11-13 22:19:23 +0800

    add shop_main recycler_recipe.xml

---
+++ app/src/main/java/com/example/baolema/RecipeAdapter.java
@@ -0,0 +1,38 @@
+package com.example.baolema;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecipeAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    static class RecipeViewHolder extends RecyclerView.ViewHolder{
+        public RecipeViewHolder(View view){
+            super(view);
+        }
+    }
+
+    public RecipeAdapter(){
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.recycleview_recipe,parent,false);
+        //将之前写好的list_view封装到一个View中
+        RecipeViewHolder holder=new RecipeViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    }
+
+    @Override
+    public int getItemCount() {
+        return 5;
+    }
+}
