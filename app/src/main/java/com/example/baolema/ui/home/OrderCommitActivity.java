5ca866a8f7181f910edd33f3f39d8dc37af9d07e
startproge <2388998657@qq.com>
2019-11-20 22:37:14 +0800

    新增了订单结算界面以及所需要的图片，颜色

---
+++ app/src/main/java/com/example/baolema/ui/home/OrderCommitActivity.java
@@ -0,0 +1,15 @@
+package com.example.baolema.ui.home;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.baolema.R;
+
+public class OrderCommitActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_commit);
+    }
+}
