f5fc728e28aed16dc200c593c652101014351cfd
俞泽勇 <31701024@stu.zucc.edu.cn>
2019-11-22 15:26:20 +0800

    "我的订单"首页

--- app/src/main/java/com/example/baolema/ui/order/OrderFragment.java
+++ app/src/main/java/com/example/baolema/ui/order/OrderFragment.java
@@ -4,20 +4,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.baolema.MainActivity;
 import com.example.baolema.R;
+import com.example.baolema.bean.OrderMain;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class OrderFragment extends Fragment {
-
     private OrderViewModel orderViewModel;
+    private List<OrderMain> ordersList = new ArrayList<>();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -26,6 +29,25 @@ public class OrderFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_order, container, false);
         MainActivity mainActivity = (MainActivity) getActivity();
         mainActivity.resetTitle("我的订单");
+
+        initOrderList();
+
+        RecyclerView recyclerView = root.findViewById(R.id.recycler_order_main);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setAdapter(new OrderMainAdapter(ordersList));
+
         return root;
+    }
+
+    public void initOrderList() {
+        ordersList.add(new OrderMain("学院学院", 32.4, "待评价"));
+        ordersList.add(new OrderMain("学院学院", 32.94, "已完成"));
+        ordersList.add(new OrderMain("学院学院", 2.94, "待评价"));
+        ordersList.add(new OrderMain("学院学院", 329.4, "待评价"));
+        ordersList.add(new OrderMain("学院学院", 32.4, "待评价"));
+        ordersList.add(new OrderMain("学院学院", 2, "待评价"));
+        ordersList.add(new OrderMain("学院学院", 32.4, "待评价"));
+        ordersList.add(new OrderMain("学院学院", 32.4, "待评价"));
+        ordersList.add(new OrderMain("学院学院", 32.4, "待评价"));
     }
 }
---
+++ app/src/main/java/com/example/baolema/ui/order/OrderMainAdapter.java
@@ -0,0 +1,64 @@
+package com.example.baolema.ui.order;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.baolema.R;
+import com.example.baolema.bean.OrderMain;
+
+import java.util.List;
+
+public class OrderMainAdapter extends RecyclerView.Adapter {
+    private List<OrderMain> orderList;
+
+    public OrderMainAdapter(List<OrderMain> orderList) {
+        this.orderList = orderList;
+    }
+
+    static class OrderMainViewHolder extends RecyclerView.ViewHolder {
+        TextView textShopName;
+        TextView textOrderStatus;
+        ImageView imageShop;
+        TextView orderSumPrice;
+        TextView orderTime;
+
+        OrderMainViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.textShopName = itemView.findViewById(R.id.text_order_main_shop);
+            this.textOrderStatus = itemView.findViewById(R.id.text_order_main_evaluate);
+            this.imageShop = itemView.findViewById(R.id.image_order_main);
+            this.orderSumPrice = itemView.findViewById(R.id.text_order_main_price);
+            this.orderTime = itemView.findViewById(R.id.text_order_main_time);
+        }
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new OrderMainViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_order_main, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        OrderMainViewHolder orderMainViewHolder = (OrderMainViewHolder) holder;
+        orderMainViewHolder.textShopName.setText(orderList.get(position).getShopName());
+        orderMainViewHolder.textOrderStatus.setText(orderList.get(position).getOrderStatus());
+        orderMainViewHolder.orderSumPrice.setText(String.valueOf(orderList.get(position).getOrderPrice()));
+//        orderMainViewHolder.imageShop.setImageBitmap();
+//        orderMainViewHolder.orderTime.setText(orderList.get(position).getOrderStartTime());
+//        shopViewHolder.shopTrademark.setImageURI(Uri.parse(shop.getShopTrademark()));
+
+        //评分显示
+    }
+
+    @Override
+    public int getItemCount() {
+        return orderList.size();
+    }
+}
