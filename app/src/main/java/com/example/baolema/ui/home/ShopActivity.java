ef9df37aa72d978990d34f210f81ec62b8298a49
fyde <2432001677@qq.com>
2019-11-22 20:24:07 +0800

    add some click things

--- app/src/main/java/com/example/baolema/ui/home/ShopActivity.java
+++ app/src/main/java/com/example/baolema/ui/home/ShopActivity.java
@@ -15,6 +15,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
